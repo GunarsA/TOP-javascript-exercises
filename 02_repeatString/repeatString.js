5dbce8746d37292dfe6ebaba99235d6e89bd1769
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-10 16:25:53 +0300

    Complete 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, repeatCount) {
+  if(repeatCount < 0) {
+    return "ERROR";
+  }
+  let combinedString = "";
+  for(let i = 0; i < repeatCount; i++) {
+    combinedString += stringToRepeat;
+  }
+  return combinedString
 };
 
 // Do not edit below this line
