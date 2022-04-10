a3596ebb3a9e7c100bb85607a16ace3ce44a3085
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-10 17:18:12 +0300

    Complete 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  for(argument of args) {
+    for(i in arr) {
+      if(argument === arr[i]) {
+        arr.splice(i, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
