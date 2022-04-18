191d8480dd86d38224eef91edcc63ea078c156d8
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-18 11:35:07 +0300

    Complete 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  if(a.length === 0) {
+    return 0;
+  }
+	return a.reduce((prev, curr) => {
+    return prev + curr;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((prev, curr) => {
+    return prev * curr;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b; 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+	for(let i = 2; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
