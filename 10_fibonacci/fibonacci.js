569d3332bcc4aa2bcc56b22b1573cd22ca97b2e7
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-18 12:00:44 +0300

    Complete 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if(n < 0) {
+    return "OOPS";
+  }
+  for(let i = 1, a = 1, b = 1;; i++) {
+    if (i == n) {
+      return (i % 2 ? a : b);
+    }
+    if(i % 2) {
+      a += b;
+    }
+    else {
+      b += a;
+    }
+  }
 };
 
 // Do not edit below this line
