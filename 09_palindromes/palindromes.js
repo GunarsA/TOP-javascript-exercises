7d323fcb0e4236206c9e3f2d0fb6e01369c7e30d
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-18 11:53:09 +0300

    Solve 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  for(let left = 0, right = string.length - 1;; left++, right--) {
+    while(!string.charAt(left).match(/[a-z]/i)) {
+      left++;
+    }
+    while(!string.charAt(right).match(/[a-z]/i)) {
+      right--;
+    }
+    if(left >= right) {
+      return true;
+    }
+    if(string.charAt(left).toLowerCase() !== string.charAt(right).toLowerCase()) {
+      return false;
+    }
+  }
 };
 
 // Do not edit below this line
