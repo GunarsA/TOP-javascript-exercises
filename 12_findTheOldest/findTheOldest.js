a386ada5ee009d773fe970d700500a95d95717b1
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-18 12:09:57 +0300

    Solve 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.sort((a, b) => {
+    const ageA = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    if(ageA > ageB) {
+      return -1;
+    }
+    if(ageA < ageB) {
+      return 1;
+    }
+    return 0;
+  })[0];
 };
 
 // Do not edit below this line
